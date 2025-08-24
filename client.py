dd6a5daeb79804cde0cf3d40f0bce8b760874c31
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-08-24 21:17:38 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,6 +2,19 @@ from info import *
 from pyrogram import Client
 from subprocess import Popen
 
+from pyrogram import utils
+
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
+
+utils.get_peer_type = get_peer_type_new
+
 User = Client(name="user", session_string=SESSION)
 DlBot = Client(name="auto-delete", 
                api_id=API_ID,
