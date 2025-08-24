9be807094948da6482d68e05aa98047b792469ac
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-08-24 20:53:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@
 API_ID       = "28809976"
 API_HASH     = "a0d9c725c9ea8b8677c361b357dfdad2"
 BOT_TOKEN    = "8404885851:AAEOMuKqK9u5T7JZwQm-jbW_saiD0KoDfck"
-SESSION      = "BAG3mvgAD2O7vPTUQkZ-qytOafYQsz44Vw_SsKOEd0R_rVc0yUejwBZ653XuxuB7mfXP5Hgw5DpjAqzEzYn8IubApp9xwjmc2x5L7IGqTwmmk4CMFvIp1Ssu8v1kjlSeWodfyTfiJuQINgvX1vzOVV5_DxAeJBcFPKS4M7DOV_k0G_KwsxqjT7R7cJoClz_e0_KH7yx3rbAD4SfUikv-xFlpfiYpmwZcg1bf8HbyG3J9C3vF5Z6-2kXUkU6hW_-pGvnO_5Pix_WOSK25bq9Z-H3oB4B4o1-AO9GXBvoeR2nNJRD_Axc-STRD-kaY4GObzCp8g-sZxlk5DEQbwz8VuCl8MqdGQAAAAAH0-GFbAQ"
+SESSION      = "BAG3mvgAKrvZ62JEJbQd-SuIh6BtYTM7OuQc3qch_qNvfbnhgqoCT-lQORldlCqubAl4JDhWs6FIluQMPVUaUD_Arp9p-cddxpMpU6411DcUMJoHlY9fatWbNj087JUGqWqrxv1IG-0z4BA6AseXFpZSJpuhVnf96_CDg-P3BrX6x2oZr6u8PXRWbo_7CbasK0RuHGg3l2UoaXqxrFqNzj88wRbqbe285uW0UszkiFSX1VMCQK-yYC1Kh5W0y5VqAd8qePCGmNvSZHs9H6_8kSKeGhfC9qh8SZ_ibXpah2ce_5h7fGl5htpA_l1m5esxuPt2nUqohkaA8gdGco7S_S30Mvj_bAAAAAF4HUkuAA"
 DATABASE_URI = "mongodb+srv://vinayjaat4:vinayjaat4@postfinder.dns5ykq.mongodb.net/?retryWrites=true&w=majority&appName=postfinder"
 LOG_CHANNEL  = -1001839965169
 ADMIN        = 5741918628
