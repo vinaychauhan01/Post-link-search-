0f0040b27b619d4b7bd4613c56ce508fadd9dbde
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-08-24 20:15:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,9 @@
 
-API_ID       = "21518327"
-API_HASH     = "e72f588b3e4763f01eecfc3c4aa7e8ac"
-BOT_TOKEN    = "7369210557:AAHye9_YGxdQOJ9yuFU3i6WkDka4CzXB4-4"
-SESSION      = "BQFIV_cAl0zQ4uSC_9SgbK4Ccx3c5hG8ppAk293kfgMetlpGh9BtSg_jxSDYOm8QYWpVjnXKHk0NdJp-KzBOc9S7hnM08XWf07HpnQakiB0EpkAfaMhT0sksPNpOO7vtNy6qmIfTuNgUfn5ROWSFZ0_nvV5t-W9WmMZ-oPZnfZSQChpHZapLjTAoblCISGCjrWDjDOFE1gl3PN_NndOYGJn9PNi7LLMSM8b8t8kDTcKehKYBB3JDCBkciqS-l8O3mesNdAtAIV4hAAshOWxp3SR5dU24cP5G5BZUxJNCI1zR0PiDW6DmbULNWmwGo0L_E2tJcNqYaHkAS9rPCATUmGrlfv0xJQAAAAFWPrmkAA"
+API_ID       = "28809976"
+API_HASH     = "a0d9c725c9ea8b8677c361b357dfdad2"
+BOT_TOKEN    = "8404885851:AAEOMuKqK9u5T7JZwQm-jbW_saiD0KoDfck"
+SESSION      = "BAG3mvgAD2O7vPTUQkZ-qytOafYQsz44Vw_SsKOEd0R_rVc0yUejwBZ653XuxuB7mfXP5Hgw5DpjAqzEzYn8IubApp9xwjmc2x5L7IGqTwmmk4CMFvIp1Ssu8v1kjlSeWodfyTfiJuQINgvX1vzOVV5_DxAeJBcFPKS4M7DOV_k0G_KwsxqjT7R7cJoClz_e0_KH7yx3rbAD4SfUikv-xFlpfiYpmwZcg1bf8HbyG3J9C3vF5Z6-2kXUkU6hW_-pGvnO_5Pix_WOSK25bq9Z-H3oB4B4o1-AO9GXBvoeR2nNJRD_Axc-STRD-kaY4GObzCp8g-sZxlk5DEQbwz8VuCl8MqdGQAAAAAH0-GFbAQ"
 DATABASE_URI = "mongodb+srv://vinayjaat4:vinayjaat4@postfinder.dns5ykq.mongodb.net/?retryWrites=true&w=majority&appName=postfinder"
 LOG_CHANNEL  = -1001839965169
 ADMIN        = 5741918628
-CHANNEL      = "supremedevelopment"
+CHANNEL      = "Animeworld_zone"
